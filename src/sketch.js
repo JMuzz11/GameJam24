44a77f9e96ba22d1d1ad32c10fc6e730b79db7fe
Travis <travis.ralph@griffithuni.edu.au>
2024-11-01 22:24:43 +1000

    Added some basic visual elements to sketch - Nothing Major

--- src/sketch.js
+++ src/sketch.js
@@ -1,8 +1,11 @@
 "use strict";
 
 
+
 function setup(){
     createCanvas(windowWidth, windowHeight);
+    console.log(windowWidth);
+    console.log(windowHeight);
 }
 
 
@@ -12,6 +15,12 @@ function windowResize() {
 
 
 function draw() {
+    //background to illustrate canvas size
+    background(50,50,50)
     windowResize();
+
+    //added just some visual elements to test the canvas
+    rect(150, 150, 100, 50)
+    rect(350, 350, 100, 50)
 
 }
