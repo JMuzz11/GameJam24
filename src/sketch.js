9ced9c9d755a5f21a5c587115a75af5134acc5d0
ae1lin <erin.duncombe@gmail.com>
2024-11-04 14:18:18 +1000

    trying to get back to main

--- src/sketch.js
+++ src/sketch.js
@@ -3,6 +3,11 @@
 
 function setup(){
     createCanvas(windowWidth, windowHeight);
+
+    //this line divides the lane from the idle resource gathering and fighter deployment visual sections
+    stroke(0,255,0);
+    strokeWeight(5);
+    line(0, windowHeight/2, windowWidth, windowHeight/2);
 }
 
 
